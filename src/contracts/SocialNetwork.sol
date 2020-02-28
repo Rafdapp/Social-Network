d1db1dc61a0fff568172d0d626b988db30430cd9
Rafus71 <rulanski71@gmail.com>
2020-02-28 23:50:18 +0000

    continue tests

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -2,8 +2,35 @@ pragma solidity ^0.5.0;
 
 contract SocialNetwork {
     string public name;
+    uint public postCount = 0;
+    mapping(uint => Post) public posts;
+
+    struct Post {
+        uint id;
+        string content;
+        uint tipAmount;
+        address author;
+    }
+
+    event PostCreated(
+        uint id,
+        string content,
+        uint tipAmount,
+        address author
+    );
 
     constructor() public {
         name = "Dapp University Social Network";
+    }
+
+    function createPost(string memory _content) public {
+        // Requre valid content
+        require(bytes(_content).length > 0);
+        // Increment post count
+        postCount ++;
+        // Create post
+        posts[postCount] = Post(postCount, _content, 0, msg.sender);
+        // trigger Event
+        emit PostCreated(postCount, _content, 0, msg.sender);    
     }
 }
--- test/SocialNetwork.test.js
+++ test/SocialNetwork.test.js
@@ -5,17 +5,62 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('SocialNetwork', (accounts) => {
+contract('SocialNetwork', ([deployer, author, tipper]) => {
     let socialNetwork
+
+    before(async () => {
+        socialNetwork = await SocialNetwork.deployed()
+    })
 
     describe('deployment', async () => {
         it('deploys successfully', async () => {
-            socialNetwork = await SocialNetwork.deployed()
             const address = await socialNetwork.address
             assert.notEqual(address, 0x0)
             assert.notEqual(address, '')
             assert.notEqual(address, null)
             assert.notEqual(address, undefined)
         })
+
+        it('has a name', async () => {
+            const name = await socialNetwork.name()
+            assert.equal(name, 'Dapp University Social Network')
+        })
+
+
     })
-}) 
+
+    describe('posts', async () => {
+        // eslint-disable-next-line no-unused-vars
+        let result, postCount
+
+        it('creates posts', async () => {
+            result = await socialNetwork.createPost('This is my first post', {
+                from: author
+            })
+            postCount = await socialNetwork.postCount()
+
+            // SUCCESS
+            assert.equal(postCount, 1)
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct')
+            assert.equal(event.content, 'This is my first post', 'content is correct')
+            assert.equal(event.tipAmount, '0', 'tip amount is correct')
+            assert.equal(event.author, author, 'author is correct')
+
+            // FAILURE: Post must have content
+            await socialNetwork.createPost('', {
+                from: author
+            }).should.be.rejected;
+
+        })
+
+        //    it('lists posts', async =>{
+        // TODO: Fill me
+        //   })
+
+        //   it('allows users to tip  posts', async =>{
+        // TODO: Fill me
+        // })
+    })
+
+})
