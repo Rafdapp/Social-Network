dc03376cd97a842712880dd23656229c49616fa9
Rafus71 <rulanski71@gmail.com>
2020-03-01 11:30:08 +0000

    continue test smart contract add function createPost and tipPost

--- test/SocialNetwork.test.js
+++ test/SocialNetwork.test.js
@@ -12,6 +12,8 @@ contract('SocialNetwork', ([deployer, author, tipper]) => {
         socialNetwork = await SocialNetwork.deployed()
     })
 
+
+
     describe('deployment', async () => {
         it('deploys successfully', async () => {
             const address = await socialNetwork.address
@@ -24,20 +26,23 @@ contract('SocialNetwork', ([deployer, author, tipper]) => {
         it('has a name', async () => {
             const name = await socialNetwork.name()
             assert.equal(name, 'Dapp University Social Network')
-        })
 
+        })
 
     })
 
     describe('posts', async () => {
         // eslint-disable-next-line no-unused-vars
         let result, postCount
-
-        it('creates posts', async () => {
+        before(async () => {
             result = await socialNetwork.createPost('This is my first post', {
                 from: author
             })
             postCount = await socialNetwork.postCount()
+        })
+
+        it('creates posts', async () => {
+
 
             // SUCCESS
             assert.equal(postCount, 1)
@@ -54,13 +59,57 @@ contract('SocialNetwork', ([deployer, author, tipper]) => {
 
         })
 
-        //    it('lists posts', async =>{
-        // TODO: Fill me
-        //   })
+        it('lists posts', async () => {
+            const post = await socialNetwork.posts(postCount)
+            assert.equal(post.id.toNumber(), postCount.toNumber(), 'id is correct')
+            assert.equal(post.content, 'This is my first post', 'content is correct')
+            assert.equal(post.tipAmount, '0', 'tip amount is correct')
+            assert.equal(post.author, author, 'author is correct')
 
-        //   it('allows users to tip  posts', async =>{
-        // TODO: Fill me
-        // })
+        })
+
+        it('allows users to tip  posts', async () => { // not pass this test but program work ok
+            //Track the author balance before purchase
+            // eslint-disable-next-line no-unused-vars
+            let oldAuthorBalance
+            oldAuthorBalance = await web3.eth.getBalance(author)
+            oldAuthorBalance = new web3.utils.BN(oldAuthorBalance)
+            // TODO: Fill me
+            result = await socialNetwork.tipPost(postCount, {
+                from: tipper,
+                value: web3.utils.toWei('1', 'Ether')
+            })
+            // SUCESS
+
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct')
+            assert.equal(event.content, 'This is my first post', 'content is correct')
+            assert.equal(event.tipAmount, '100000000000000000', 'tip amount is correct')
+            assert.equal(event.author, author, 'author is correct')
+
+            // Check that author recived funds
+
+            let newAuthorBalance
+            newAuthorBalance = await web3.eth.getBalance(author)
+            newAuthorBalance = new web3.utils.BN(newAuthorBalance)
+
+
+            let tipAmount
+            tipAmount = web3.utils.toWei('1', 'Ether')
+            tipAmount = new web3.utils.BN(tipAmount)
+
+
+            const expectedBalance = oldAuthorBalance.add(tipAmount)
+
+            assert.equal(newAuthorBalance.toString(), expectedBalance.toString())
+
+            // FAILURE: tries to tip post that does not exist 
+
+            await socialNetwork.tipPost(99, {
+                from: tipper,
+                value: web.utils.toWei('1', 'Ether')
+            }).should.be.rejected;
+        })
     })
 
 })
